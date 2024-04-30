ed23f3de439ee5c6c020ba7a9a45e8b4d865cc18
lydia-l3 <122927604+lydia-l3@users.noreply.github.com>
2024-04-30 08:06:48 +0000

    added eldest and youngest# final commit

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -9,7 +9,19 @@ class DirectorsController < ApplicationController
     matching_records = Director.where({ :id => the_id })
 
     @the_director = matching_records.at(0)
-    
+
     render({ :template => "director_templates/details" })
+  end
+
+  def junior
+    @youngest_director = Director.where.not(dob: nil).order(dob: :desc).first
+
+    render({ :template => "director_templates/youngest" })
+  end
+
+  def senior
+    @eldest_director = Director.where.not(dob: nil).order(dob: :asc).first
+
+    render({ :template => "director_templates/eldest" })
   end
 end
